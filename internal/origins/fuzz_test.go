8ce6f3332bb1c7386f63a0ca8386e69734e409f3
jub0bs <jcretel-infosec+github@protonmail.com>
2024-08-30 10:44:49 +0200

    fuzz Parse against net.SplitHostPort (fixes #3)

--- internal/origins/fuzz_test.go
+++ internal/origins/fuzz_test.go
@@ -1,6 +1,8 @@
 package origins
 
 import (
+	"net"
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -22,6 +24,46 @@ func FuzzConsistencyBetweenParsePatternAndParse(f *testing.F) {
 		if _, ok := Parse(raw); !ok {
 			const tmpl = "pattern without wildcard %q fails to parse as an origin"
 			t.Errorf(tmpl, raw)
+		}
+	})
+}
+
+func FuzzConsistencyBetweenParseAndSplitHostPort(f *testing.F) {
+	for _, c := range parseCases {
+		f.Add(c.input)
+	}
+	f.Fuzz(func(t *testing.T, raw string) {
+		origin, ok := Parse(raw)
+		if !ok {
+			t.Skip()
+		}
+		// Elide scheme and scheme-host separator.
+		_, hostPort, found := strings.Cut(raw, "://")
+		if !found {
+			t.Skip()
+		}
+		// Check whether the host is valid;
+		// Parse is lenient about this but,
+		// if both Parse and parseHostPattern succeed,
+		// then we expect the host to be valid.
+		_, _, err := parseHostPattern(hostPort, hostPort)
+		if err != nil {
+			t.Skip()
+		}
+		// Check the port; if it's absent, skip this case,
+		// since net.SplitHostPort would choke on it.
+		if origin.Port == 0 {
+			t.Skip()
+		}
+		wantHost, wantPort, err := net.SplitHostPort(hostPort)
+		if err != nil {
+			t.Fatal(err)
+		}
+		gotHost := origin.Host.Value
+		gotPort := strconv.FormatInt(int64(origin.Port), 10)
+		if gotHost != wantHost || gotPort != wantPort {
+			const tmpl = "(host, port) of %q: got (%q, %s); want (%q, %s)"
+			t.Errorf(tmpl, hostPort, gotHost, gotPort, wantHost, wantPort)
 		}
 	})
 }
