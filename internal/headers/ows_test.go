701c1e146d68b112f6b05a9c618a42ceadfcec37
jub0bs <jcretel-infosec+github@protonmail.com>
2025-04-23 14:58:46 +0200

    add benchmarks for TrimOWS

--- internal/headers/ows_test.go
+++ internal/headers/ows_test.go
@@ -6,57 +6,59 @@ import (
 	"github.com/jub0bs/cors/internal/headers"
 )
 
+const maxOWSBytes = 1
+
+var trimOWStests = []struct {
+	desc string
+	s    string
+	want string
+	ok   bool
+}{
+	{
+		desc: "empty",
+		s:    "",
+		want: "",
+		ok:   true,
+	}, {
+		desc: "no OWS",
+		s:    "foo",
+		want: "foo",
+		ok:   true,
+	}, {
+		desc: "internal OWS",
+		s:    "foo  \t\tbar",
+		want: "foo  \t\tbar",
+		ok:   true,
+	}, {
+		desc: "leading and trailing OWS",
+		s:    "\tfoo ",
+		want: "foo",
+		ok:   true,
+	}, {
+		desc: "too much leading OWS",
+		s:    " \tfoo\t",
+		want: " \tfoo\t",
+		ok:   false,
+	}, {
+		desc: "too much trailing OWS",
+		s:    " foo\t ",
+		want: " foo\t ",
+		ok:   false,
+	}, {
+		desc: "too much leading and trailing OWS",
+		s:    " \tfoo\t ",
+		want: " \tfoo\t ",
+		ok:   false,
+	}, {
+		desc: "non-OWS whitespace",
+		s:    "\nfoo\t",
+		want: "\nfoo",
+		ok:   true,
+	},
+}
+
 func TestTrimOWS(t *testing.T) {
-	const maxOWSBytes = 1
-	cases := []struct {
-		desc string
-		s    string
-		want string
-		ok   bool
-	}{
-		{
-			desc: "empty",
-			s:    "",
-			want: "",
-			ok:   true,
-		}, {
-			desc: "no OWS",
-			s:    "foo",
-			want: "foo",
-			ok:   true,
-		}, {
-			desc: "internal OWS",
-			s:    "foo  \t\tbar",
-			want: "foo  \t\tbar",
-			ok:   true,
-		}, {
-			desc: "leading and trailing OWS",
-			s:    "\tfoo ",
-			want: "foo",
-			ok:   true,
-		}, {
-			desc: "too much leading OWS",
-			s:    " \tfoo\t",
-			want: " \tfoo\t",
-			ok:   false,
-		}, {
-			desc: "too much trailing OWS",
-			s:    " foo\t ",
-			want: " foo\t ",
-			ok:   false,
-		}, {
-			desc: "too much leading and trailing OWS",
-			s:    " \tfoo\t ",
-			want: " \tfoo\t ",
-			ok:   false,
-		}, {
-			desc: "non-OWS whitespace",
-			s:    "\nfoo\t",
-			want: "\nfoo",
-			ok:   true,
-		},
-	}
-	for _, tc := range cases {
+	for _, tc := range trimOWStests {
 		f := func(t *testing.T) {
 			got, ok := headers.TrimOWS(tc.s, maxOWSBytes)
 			if ok != tc.ok || got != tc.want {
@@ -65,5 +67,16 @@ func TestTrimOWS(t *testing.T) {
 			}
 		}
 		t.Run(tc.desc, f)
+	}
+}
+
+func BenchmarkTrimOWS(b *testing.B) {
+	for _, tc := range trimOWStests {
+		f := func(b *testing.B) {
+			for range b.N {
+				headers.TrimOWS(tc.s, maxOWSBytes)
+			}
+		}
+		b.Run(tc.desc, f)
 	}
 }
