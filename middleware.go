b7fb670408f321c0dbd4a0860a85d20f6851ea64
jub0bs <jcretel-infosec+github@protonmail.com>
2025-09-07 11:56:34 +0200

    document why a single method is listed in ACAM

--- middleware.go
+++ middleware.go
@@ -357,6 +357,16 @@ func (icfg *internalConfig) processACRM(
 		// Therefore, no need to set the ACAM header in this case.
 		return true
 	}
+	// Note that middleware only ever list a single method in the ACAM header.
+	// One inconvenient of this behavior is that it leads to less than ideal
+	// caching by the browser of responses to CORS-preflight requests;
+	// see https://fetch.spec.whatwg.org/#cors-preflight-cache.
+	// However, this behavior presents two advantages with respect to responses
+	// to CORS-preflight requests:
+	//   - those responses disclose no other allowed methods than the one
+	//     required for preflight to succeed; and
+	//   - those responses are smaller than they would otherwise be,
+	//     thereby saving some bandwidth.
 	if icfg.allowAnyMethod && !icfg.credentialed {
 		buf[headers.ACAM] = headers.WildcardSgl
 		return true
