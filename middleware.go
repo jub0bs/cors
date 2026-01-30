3963c2faeb4fcddf0ccc5fcb67d5e0d34b7e7952
jub0bs <jcretel-infosec+github@protonmail.com>
2026-01-30 15:35:33 +0100

    further simplify (*internalConfig) processACRH

--- middleware.go
+++ middleware.go
@@ -396,62 +396,49 @@ func (icfg *internalConfig) processACRH(
 	if !found {
 		return true
 	}
-	if icfg.asteriskReqHdrs {
-		if icfg.credentialed {
-			// If credentialed access is enabled,
-			// the single-asterisk pattern denotes all request-header names,
-			// including Authorization.
-			// Therefore, users of jub0bs/cors cannot both
-			// allow all request-header names other than Authorization
-			// and allow credentialed access.
-			// This limitation is the result of a deliberate design choice.
-			//
-			// First, rare are the cases where all request-header names
-			// other than Authorization should be allowed
-			// with credentialed access enabled.
-			//
-			// Second, because jub0bs/cors prohibits its users from
-			// allowing all origins with credentialed access,
-			// allowing all request headers from select origins along
-			// with credentialed access presents little security-related risks.
-			//
-			// Third, if we followed an alternative approach
-			// in which * doesn't cover Authorization,
-			// we would need to scan the ACRH header in search of "authorization";
-			// as explained in an implementation comment above,
-			// such a computation would introduce performance issues.
-			// Moreover, if "authorization" were found in ACRH,
-			// we couldn't simply echo ACRH in ACAH,
-			// because we'd have to omit "authorization" in ACAH.
-			// Incidentally, this could be achieved
-			// without incurring heap allocations,
-			// e.g. by cutting ACRH around "authorization" and
-			// echoing the results in up to two ACAH header(s);
-			// but the whole alternative approach is not worth the trouble anyway.
-			//
-			// We can simply reflect all the ACRH header lines as ACAH header lines
-			// because the Fetch standard requires browsers to handle multiple ACAH
-			// header lines;
-			// see https://fetch.spec.whatwg.org/#cors-preflight-fetch-0.
-			//
-			// Reflecting ACRH into ACAH isn't ideal for performance in cases where
-			// ACRH is full of junk, but there isn't much else we can do, other than
-			// discourage users from both enabling credentialed access and allowing
-			// all request-header names.
-			buf[headers.ACAH] = acrh
-		} else {
-			buf[headers.ACAH] = icfg.acah
-		}
+
+	switch {
+	case icfg.asteriskReqHdrs && icfg.credentialed:
+		// If credentialed access is enabled,
+		// the single-asterisk pattern denotes all request-header names,
+		// including Authorization.
+		// Therefore, users of jub0bs/cors cannot both
+		// allow all request-header names other than Authorization
+		// and allow credentialed access.
+		// This limitation is the result of a deliberate design choice.
+		//
+		// First, rare are the cases where all request-header names
+		// other than Authorization should be allowed
+		// with credentialed access enabled.
+		//
+		// Second, because jub0bs/cors prohibits its users from
+		// allowing all origins with credentialed access,
+		// allowing all request headers from select origins along
+		// with credentialed access presents little security-related risks.
+		//
+		// Third, if we followed an alternative approach
+		// in which * doesn't cover Authorization,
+		// we would need to scan the ACRH header in search of "authorization";
+		// as explained in an implementation comment above,
+		// such a computation would introduce performance issues.
+		// Moreover, if "authorization" were found in ACRH,
+		// we couldn't simply echo ACRH in ACAH,
+		// because we'd have to omit "authorization" in ACAH.
+		// Incidentally, this could be achieved
+		// without incurring heap allocations,
+		// e.g. by cutting ACRH around "authorization" and
+		// echoing the results in up to two ACAH header(s);
+		// but the whole alternative approach is not worth the trouble anyway.
+		break
+	case icfg.asteriskReqHdrs && !icfg.credentialed:
+		buf[headers.ACAH] = icfg.acah
 		return true
-	}
-	if debug {
-		if icfg.acah != nil {
-			buf[headers.ACAH] = icfg.acah
-			return true
-		}
+	case debug && icfg.acah != nil:
+		buf[headers.ACAH] = icfg.acah
+		return true
+	case debug && icfg.acah == nil:
 		return false
-	}
-	if !headers.Check(icfg.allowedReqHdrs, acrh) {
+	case !headers.Check(icfg.allowedReqHdrs, acrh):
 		return false
 	}
 	// We can simply reflect all the ACRH header lines as ACAH header lines
