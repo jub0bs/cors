b9f053e6d1d6ebdcc0f771fc14188c06cef80dc8
jub0bs <jcretel-infosec+github@protonmail.com>
2026-02-27 12:46:39 +0100

    assert that portOffset == math.MaxUint16 + 2

--- internal/origins/compile_time_test.go
+++ internal/origins/compile_time_test.go
@@ -6,5 +6,7 @@ var _ [absentPort][-absentPort]struct{} // => absentPort == 0
 
 var _ [arbitraryPort + 1][-arbitraryPort - 1]struct{} // => arbitraryPort == -1
 
-// Check that portOffset straddles the entire range of Pattern.Port values.
-var _ [portOffset - math.MaxUint16 - 2]struct{} // portOffset >= math.MaxUint16 + 2
+// Check that portOffset == math.MaxUint16 + 2. Hence, portOffset "straddles"
+// (and exactly so) the entire range of valid Pattern.Port values, i.e.
+// [-1, math.MaxUint16].
+var _ [portOffset - math.MaxUint16 - 2][-portOffset + math.MaxUint16 + 2]struct{}
