2639eb63bcc6a1155e473e5612295971669502f7
jub0bs <jcretel-infosec+github@protonmail.com>
2026-08-14 10:52:40 +0200

    shorten name of receiver in sortedset.Set methods

--- internal/sortedset/sortedset.go
+++ internal/sortedset/sortedset.go
@@ -13,78 +13,73 @@ type Set struct {
 	maxLen int
 }
 
-// Add adds e to set. Calling Add generally breaks set's invariants.
-func (set *Set) Add(e string) {
-	set.elems = append(set.elems, e)
+// Add adds e to s. Calling Add generally breaks the invariants of s.
+func (s *Set) Add(e string) {
+	s.elems = append(s.elems, e)
 }
 
-// Fix re-establishes set's invariants.
-func (set *Set) Fix() {
-	slices.Sort(set.elems)
-	set.elems = slices.Compact(set.elems)
-	for _, e := range set.elems {
-		set.maxLen = max(set.maxLen, len(e))
+// Fix re-establishes the invariants of s.
+func (s *Set) Fix() {
+	slices.Sort(s.elems)
+	s.elems = slices.Compact(s.elems)
+	for _, e := range s.elems {
+		s.maxLen = max(s.maxLen, len(e))
 	}
 }
 
-// Size returns the cardinality of set.
+// Size returns the cardinality of s.
 //
-// Precondition: [*Set.Add] was not called since [*Set.Fix] was
-// last called.
-func (set Set) Size() int {
-	return len(set.elems)
+// Precondition: [*Set.Add] was not called since [*Set.Fix] was last called.
+func (s Set) Size() int {
+	return len(s.elems)
 }
 
-// MaxLen returns the length of set's longest element, or 0 if set is empty.
+// MaxLen returns the length of the longest element of s, or 0 if s is empty.
 //
-// Precondition: [*Set.Add] was not called since [*Set.Fix] was
-// last called.
-func (set Set) MaxLen() uint {
-	return uint(set.maxLen)
+// Precondition: [*Set.Add] was not called since [*Set.Fix] was last called.
+func (s Set) MaxLen() uint {
+	return uint(s.maxLen)
 }
 
-// Contains reports whether e is an element of set.
+// Contains reports whether e is an element of s.
 //
-// Precondition: [*Set.Add] was not called since [*Set.Fix] was
-// last called.
-func (set Set) Contains(e string) bool {
-	return set.Index(0, e) >= 0
+// Precondition: [*Set.Add] was not called since [*Set.Fix] was last called.
+func (s Set) Contains(e string) bool {
+	return s.Index(0, e) >= 0
 }
 
-// Index returns the index of e in set if it occurs after the first i elements
-// of set, or -1 otherwise.
+// Index returns the index of e in s if it occurs after the first i elements of
+// s, or -1 otherwise.
 //
-// Precondition: [*Set.Add] was not called since [*Set.Fix] was
-// last called.
-func (set Set) Index(i uint, e string) int {
-	l := uint(len(set.elems))
-	if len(e) > set.maxLen || i >= l {
+// Precondition: [*Set.Add] was not called since [*Set.Fix] was last called.
+func (s Set) Index(i uint, e string) int {
+	l := uint(len(s.elems))
+	if len(e) > s.maxLen || i >= l {
 		return -1
 	}
-	// Let's binary-search for e in set.elems[i:]. We eschew
+	// Let's binary-search for e in s.elems[i:]. We eschew
 	// slices.BinarySearch here, so as to keep the method inlineable.
 	for j := l; i < j; {
 		h := (i + j) >> 1
 		// The length check below is redundant, but it's useful because it
 		// eliminates the bounds check for h.
-		if h < l && set.elems[h] < e {
+		if h < l && s.elems[h] < e {
 			i = h + 1
 		} else {
 			j = h
 		}
 	}
-	if i >= l || set.elems[i] != e {
+	if i >= l || s.elems[i] != e {
 		return -1
 	}
 	// The following uint-to-int conversion is safe because
-	// i < len(set.elems) <= math.MaxInt.
+	// i < len(s.elems) <= math.MaxInt.
 	return int(i)
 }
 
-// ToSlice returns a slice of set's elements sorted in lexicographical order.
+// ToSlice returns a slice of s's elements sorted in lexicographical order.
 //
-// Precondition: [*Set.Add] was not called since [*Set.Fix] was
-// last called.
-func (set Set) ToSlice() []string {
-	return slices.Clone(set.elems) // defensive copying
+// Precondition: [*Set.Add] was not called since [*Set.Fix] was last called.
+func (s Set) ToSlice() []string {
+	return slices.Clone(s.elems) // defensive copying
 }
